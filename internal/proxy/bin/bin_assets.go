acc60d66fe5a1af6c0fb56929656a2207bbf0efa
TJ Holowaychuk <tj@vision-media.ca>
2017-08-07 17:04:38 -0700

    rebuild proxy

--- internal/proxy/bin/bin_assets.go
+++ internal/proxy/bin/bin_assets.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25ef4feeecb42badc4335d6b5f5d10fd85057f46839e3386d14952e90383fa62
-size 11875692
+oid sha256:653513ba5b97be6d2e1ec2aaf46537e9378cd0b4bff2b8da39635be431e5c303
+size 11877165
