116ddb63926b970517552c368e01022280d6e2ac
TJ Holowaychuk <tj@vision-media.ca>
2017-08-08 09:29:15 -0700

    fix `version` description

--- internal/cli/version/version.go
+++ internal/cli/version/version.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 
 	"github.com/tj/kingpin"
+
 	"github.com/apex/up/internal/cli/root"
 )
 
 func init() {
-	cmd := root.Command("version", "Output version.")
+	cmd := root.Command("version", "Show version.")
 	cmd.Action(func(_ *kingpin.ParseContext) error {
 		fmt.Println(root.Cmd.GetVersion())
 		return nil
