289ddc252a5b881be17ba316a18319a757314758
TJ Holowaychuk <tj@vision-media.ca>
2017-08-15 11:37:58 -0700

    remove old ./server defaults as they assume source files are present in Lambda

--- config.go
+++ config.go
@@ -291,10 +291,6 @@ func ReadConfig(path string) (*Config, error) {
 
 // golang config.
 func golang(c *Config) {
-	if c.Proxy.Command == "" {
-		c.Proxy.Command = "./server"
-	}
-
 	if c.Hooks.Build == "" {
 		c.Hooks.Build = `GOOS=linux GOARCH=amd64 go build -o server *.go`
 	}
@@ -306,10 +302,6 @@ func golang(c *Config) {
 
 // crystal config.
 func crystal(c *Config) {
-	if c.Proxy.Command == "" {
-		c.Proxy.Command = "./server"
-	}
-
 	if c.Hooks.Build == "" {
 		c.Hooks.Build = `docker run --rm -v $(PWD):/src -w /src tjholowaychuk/up-crystal crystal build --link-flags -static -o server main.cr`
 	}
