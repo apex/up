56de492bd725a54c095ae782b7e70d23a43ef2a2
Francisco Guimarães <francisco.cpg@gmail.com>
2017-08-21 11:53:18 -0700

    add support for regions defined in ~/.aws/config

--- config.go
+++ config.go
@@ -16,6 +16,7 @@ import (
 	"github.com/apex/up/internal/util"
 	"github.com/apex/up/internal/validate"
 	"github.com/apex/up/platform/lambda/regions"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 // TODO: refactor defaulting / validation with slices
@@ -218,15 +219,23 @@ func (c *Config) defaultRegions() error {
 		return nil
 	}
 
-	if s := os.Getenv("AWS_REGION"); s != "" {
-		log.Debugf("region from AWS_REGION %q", s)
-		c.Regions = append(c.Regions, s)
+	s, err := session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	})
+
+	if err != nil {
+		return errors.Wrap(err, "creating session")
+	}
+
+	if r := *s.Config.Region; r != "" {
+		log.Debugf("region from aws shared config %q", r)
+		c.Regions = append(c.Regions, r)
 		return nil
 	}
 
-	s := "us-west-2"
-	log.Debugf("region defaulted to %q", s)
-	c.Regions = append(c.Regions, s)
+	r := "us-west-2"
+	log.Debugf("region defaulted to %q", r)
+	c.Regions = append(c.Regions, r)
 	return nil
 }
 
--- config_test.go
+++ config_test.go
@@ -1,6 +1,8 @@
 package up
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/tj/assert"
@@ -62,6 +64,17 @@ func TestConfig_Type(t *testing.T) {
 }
 
 func TestConfig_Regions(t *testing.T) {
+	t.Run("valid multiple", func(t *testing.T) {
+		c := Config{
+			Name:    "api",
+			Type:    "server",
+			Regions: []string{"us-west-2", "us-east-1"},
+		}
+
+		assert.NoError(t, c.Default(), "default")
+		assert.NoError(t, c.Validate(), "validate")
+	})
+
 	t.Run("valid multiple", func(t *testing.T) {
 		c := Config{
 			Name:    "api",
@@ -135,5 +148,132 @@ func TestConfig_Regions(t *testing.T) {
   • ap-south-1
   • sa-east-1
   • ca-central-1`)
+	})
+}
+
+func TestConfig_defaultRegions(t *testing.T) {
+	t.Run("regions from config", func(t *testing.T) {
+		regions := []string{"us-west-2", "us-east-1"}
+		c := Config{
+			Name:    "api",
+			Type:    "server",
+			Regions: regions,
+		}
+
+		assert.NoError(t, c.defaultRegions(), "defaultRegions")
+		assert.Equal(t, 2, len(c.Regions), "regions should have length 2")
+		assert.Equal(t, regions, c.Regions, "should read regions from config")
+		assert.NoError(t, c.Validate(), "validate")
+	})
+
+	t.Run("regions from AWS_REGION", func(t *testing.T) {
+		region := "sa-east-1"
+		os.Setenv("AWS_REGION", region)
+
+		defer os.Setenv("AWS_REGION", "")
+		c := Config{
+			Name: "api",
+			Type: "server",
+		}
+
+		assert.NoError(t, c.defaultRegions(), "defaultRegions")
+		assert.Equal(t, 1, len(c.Regions), "regions should have length 1")
+		assert.Equal(t, region, c.Regions[0], "should read regions from AWS_REGION")
+		assert.NoError(t, c.Validate(), "validate")
+	})
+
+	t.Run("regions from AWS_DEFAULT_REGION", func(t *testing.T) {
+		region := "sa-east-1"
+
+		os.Setenv("AWS_DEFAULT_REGION", region)
+		defer os.Setenv("AWS_DEFAULT_REGION", "")
+
+		c := Config{
+			Name: "api",
+			Type: "server",
+		}
+
+		assert.NoError(t, c.defaultRegions(), "defaultRegions")
+		assert.Equal(t, 1, len(c.Regions), "regions should have length 1")
+		assert.Equal(t, region, c.Regions[0], "should read regions from AWS_DEFAULT_REGION")
+		assert.NoError(t, c.Validate(), "validate")
+	})
+
+	t.Run("regions from shared config with default profile", func(t *testing.T) {
+		content := `
+		[default]
+		region = sa-east-1
+		output = json
+		[profile another-profile]
+		region = ap-southeast-2
+		output = json`
+
+		tmpfile, err := ioutil.TempFile("", "config")
+		assert.NoError(t, err)
+		defer os.Remove(tmpfile.Name())
+
+		_, err = tmpfile.WriteString(content)
+		assert.NoError(t, err)
+
+		os.Setenv("AWS_CONFIG_FILE", tmpfile.Name())
+		defer os.Setenv("AWS_CONFIG_FILE", "")
+
+		c := Config{
+			Name: "api",
+			Type: "server",
+		}
+
+		assert.NoError(t, c.defaultRegions(), "defaultRegions")
+		assert.Equal(t, 1, len(c.Regions), "regions should have length 1")
+		assert.Equal(t, "sa-east-1", c.Regions[0], "should read regions from shared config with default profile")
+		assert.NoError(t, c.Validate(), "validate")
+	})
+
+	t.Run("regions from shared config with AWS_PROFILE profile", func(t *testing.T) {
+		content := `
+		[default]
+		region = sa-east-1
+		output = json
+		[profile another-profile]
+		region = ap-southeast-2
+		output = json`
+
+		tmpfile, err := ioutil.TempFile("", "config")
+		assert.NoError(t, err)
+		defer os.Remove(tmpfile.Name())
+
+		_, err = tmpfile.WriteString(content)
+		assert.NoError(t, err)
+
+		os.Setenv("AWS_CONFIG_FILE", tmpfile.Name())
+		defer os.Setenv("AWS_CONFIG_FILE", "")
+
+		os.Setenv("AWS_PROFILE", "another-profile")
+		defer os.Setenv("AWS_PROFILE", "")
+
+		c := Config{
+			Name: "api",
+			Type: "server",
+		}
+
+		assert.NoError(t, c.defaultRegions(), "defaultRegions")
+		assert.Equal(t, 1, len(c.Regions), "regions should have length 1")
+		assert.Equal(t, "ap-southeast-2", c.Regions[0], "should read regions from shared config with AWS_PROFILE profile")
+		assert.NoError(t, c.Validate(), "validate")
+	})
+
+	t.Run("default region must be us-west-2", func(t *testing.T) {
+		// Make sure we aren't reading AWS config file
+		os.Setenv("AWS_CONFIG_FILE", "does-not-exist")
+
+		c := Config{
+			Name: "api",
+			Type: "server",
+		}
+
+		assert.NoError(t, c.defaultRegions(), "defaultRegions")
+		assert.Equal(t, 1, len(c.Regions), "regions should have length 1")
+		assert.Equal(t, "us-west-2", c.Regions[0], "default region must be us-west-2")
+		assert.NoError(t, c.Validate(), "validate")
 	})
 }
