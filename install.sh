543265396d7f10449c07d613274b0937b490383c
quocnguyen <quocnguyen@users.noreply.github.com>
2017-09-17 01:08:58 +0700

    fix 404 checksum not found (#331)

--- install.sh
+++ install.sh
@@ -304,7 +304,7 @@ echo "$PREFIX: found version ${VERSION} for ${OS}/${ARCH}"
 NAME=${BINARY}_${VERSION}_${OS}_${ARCH}
 TARBALL=${NAME}.${FORMAT}
 TARBALL_URL=${GITHUB_DOWNLOAD}/v${VERSION}/${TARBALL}
-CHECKSUM=${BINARY}_checksums.txt
+CHECKSUM=${BINARY}_${VERSION}_checksums.txt
 CHECKSUM_URL=${GITHUB_DOWNLOAD}/v${VERSION}/${CHECKSUM}
 
 # Adjust binary name if windows
