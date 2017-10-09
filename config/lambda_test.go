a92b2d7cfff37a8565459737cf4e45ef2842c48e
TJ Holowaychuk <tj@vision-media.ca>
2017-10-09 15:18:48 +0300

    fix lambda config test

--- config/lambda_test.go
+++ config/lambda_test.go
@@ -9,6 +9,6 @@ import (
 func TestLambda(t *testing.T) {
 	c := &Lambda{}
 	assert.NoError(t, c.Default(), "default")
-	assert.Equal(t, 15, c.Timeout, "timeout")
+	assert.Equal(t, 0, c.Timeout, "timeout")
 	assert.Equal(t, 512, c.Memory, "timeout")
 }
