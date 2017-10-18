apex/up

commit e8a33a3b2862ea171b6ec4f552c96343cd4c8ea0

fix case where improper cert is created due to second-level domain (.co.uk). Closes #350
not ideal, but it is a hotfix for now

platform/lambda/lambda.go
@@ -278,10 +278,6 @@ func (p *Platform) ApplyStack(region string) error {
 // the HostedZoneId if present. This is required because domains
 // purchased via Route53 will already have a hosted zone,
 // so we need to reference it instead of creating a new zone.
-//
-// Currently subdomains are stripped. For example if you create
-// an app which maps only production to "api.example.com", the
-// zone "example.com" will be created (or discovered).
 func (p *Platform) populateZones() error {
 	r := route53.New(session.New(aws.NewConfig()))
 
@@ -294,10 +290,9 @@ func (p *Platform) populateZones() error {
 	}
 
 	for _, s := range p.config.Stages.List() {
-		domain := util.Domain(s.Domain)
-		log.Debugf("finding stage dns zones for %s %s (%s)", s.Name, domain, s.Domain)
+		log.Debugf("finding stage dns zones for %s %s", s.Name, s.Domain)
 		for _, z := range res.HostedZones {
-			if domain+"." == *z.Name {
+			if s.Domain+"." == *z.Name {
 				s.HostedZoneID = strings.Replace(*z.Id, "/hostedzone/", "", 1)
 				log.Debugf("found existing dns zone %s (%s) mapped to stage %s", s.Domain, s.HostedZoneID, s.Name)
 			}
@@ -334,18 +329,9 @@ func (p *Platform) createCerts() error {
 			continue
 		}
 
-		domain := util.Domain(s.Domain)
-
-		// already requested
-		if util.StringsContains(domains, domain) {
-			log.Debugf("already requested cert for %s", domain)
-			continue
-		}
-
-		// see if the cert exists, stripping any subdomain
-		// since we request only a single wildcard cert.
+		// see if the cert exists
 		log.Debugf("looking up cert for %s", s.Domain)
-		arn := getCert(res.CertificateSummaryList, domain)
+		arn := getCert(res.CertificateSummaryList, s.Domain)
 		if arn != "" {
 			log.Debugf("found cert for %s: %s", s.Domain, arn)
 			s.Cert = arn
@@ -354,15 +340,14 @@ func (p *Platform) createCerts() error {
 
 		// request the cert
 		res, err := a.RequestCertificate(&acm.RequestCertificateInput{
-			DomainName:              &domain,
-			SubjectAlternativeNames: aws.StringSlice([]string{"*." + domain}),
+			DomainName: &s.Domain,
 		})
 
 		if err != nil {
 			return errors.Wrapf(err, "requesting cert for %s", s.Domain)
 		}
 
-		domains = append(domains, domain)
+		domains = append(domains, s.Domain)
 		s.Cert = *res.CertificateArn
 	}
 

internal/util/util.go
@@ -213,17 +213,6 @@ func ExitStatus(cmd *exec.Cmd, err error) string {
 	return "?"
 }
 
-// IsSubdomain returns true if s is a subdomain.
-func IsSubdomain(s string) bool {
-	return len(strings.Split(s, ".")) > 2
-}
-
-// Domain returns the domain devoid of any subdomain(s).
-func Domain(s string) string {
-	p := strings.Split(s, ".")
-	return strings.Join(p[len(p)-2:], ".")
-}
-
 // StringsContains returns true if list contains s.
 func StringsContains(list []string, s string) bool {
 	for _, v := range list {

internal/util/util_test.go
@@ -26,17 +26,3 @@ func TestExitStatus(t *testing.T) {
 		assert.Equal(t, "5", code)
 	})
 }
-
-func TestIsSubdomain(t *testing.T) {
-	assert.Equal(t, false, IsSubdomain("gh-polls.com"))
-	assert.Equal(t, true, IsSubdomain("staging.gh-polls.com"))
-	assert.Equal(t, true, IsSubdomain("blog.gh-polls.com"))
-	assert.Equal(t, true, IsSubdomain("foo.bar.gh-polls.com"))
-}
-
-func TestDomain(t *testing.T) {
-	assert.Equal(t, "gh-polls.com", Domain("gh-polls.com"))
-	assert.Equal(t, "gh-polls.com", Domain("staging.gh-polls.com"))
-	assert.Equal(t, "gh-polls.com", Domain("blog.gh-polls.com"))
-	assert.Equal(t, "gh-polls.com", Domain("foo.api.gh-polls.com"))
-}

reporter/text.go
@@ -169,7 +169,7 @@ func (r *reporter) Start() {
 				fmt.Printf("\n")
 			case "platform.certs.create":
 				domains := e.Fields["domains"].([]string)
-				r.log("domains", "Verify your email")
+				r.log("domains", "Check your email for certificate approval")
 				r.pending("confirm", strings.Join(domains, ", "))
 			case "platform.certs.create.complete":
 				r.complete("confirm", "complete", e.Duration("duration"))

commit cb03c2ea4f2dd101df42f7afe64c7d50ac0b1da2

fix hosted zone regression introduced by e8a33a3
was creating a zone per. could use some better testing here

platform/lambda/lambda.go
@@ -275,9 +275,13 @@ func (p *Platform) ApplyStack(region string) error {
 }
 
 // populateZones fetches the existing hosted zones, storing
-// the HostedZoneId if present. This is required because domains
-// purchased via Route53 will already have a hosted zone,
-// so we need to reference it instead of creating a new zone.
+// the HostedZoneId if present.
+//
+// This is required because domains purchased via Route53
+// will already have a hosted zone, and we may already have
+// a hosted zone for the apex (example.com) and we are
+// creating a sub-domain (api.example.com), these should
+// all live in the same zone.
 func (p *Platform) populateZones() error {
 	r := route53.New(session.New(aws.NewConfig()))
 
@@ -292,9 +296,9 @@ func (p *Platform) populateZones() error {
 	for _, s := range p.config.Stages.List() {
 		log.Debugf("finding stage dns zones for %s %s", s.Name, s.Domain)
 		for _, z := range res.HostedZones {
-			if s.Domain+"." == *z.Name {
+			if strings.Contains(s.Domain+".", *z.Name) {
 				s.HostedZoneID = strings.Replace(*z.Id, "/hostedzone/", "", 1)
-				log.Debugf("found existing dns zone %s (%s) mapped to stage %s", s.Domain, s.HostedZoneID, s.Name)
+				log.Debugf("found existing dns zone %s (%s)", *z.Name, s.HostedZoneID)
 			}
 		}
 	}