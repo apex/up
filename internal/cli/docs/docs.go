3f03e8bd97a88579606cab88d232905bfbdf81c8
TJ Holowaychuk <tj@vision-media.ca>
2017-11-17 13:22:58 -0800

    refactor: add "Open Docs" track call

--- internal/cli/docs/docs.go
+++ internal/cli/docs/docs.go
@@ -5,16 +5,17 @@ import (
 	"github.com/tj/kingpin"
 
 	"github.com/apex/up/internal/cli/root"
+	"github.com/apex/up/internal/stats"
 )
 
 var url = "https://up.docs.apex.sh"
 
 func init() {
 	cmd := root.Command("docs", "Open documentation website in the browser.")
-
 	cmd.Example(`up docs`, "Open the documentation site.")
 
 	cmd.Action(func(_ *kingpin.ParseContext) error {
+		stats.Track("Open Docs", nil)
 		return browser.OpenURL(url)
 	})
 }
