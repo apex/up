cea76bb028bbc66f504bce7d26d6393d31d68e55
TJ Holowaychuk <tj@vision-media.ca>
2017-11-21 10:54:41 -0800

    refactor: add props to stack Track apply call

--- internal/cli/stack/stack.go
+++ internal/cli/stack/stack.go
@@ -59,6 +59,8 @@ func apply(cmd *kingpin.CmdClause) {
 			"dns_zone_count":     len(c.DNS.Zones),
 			"stage_count":        len(c.Stages.List()),
 			"stage_domain_count": len(c.Stages.Domains()),
+			"alerts_count":       len(c.Alerts),
+			"actions_count":      len(c.Actions),
 		})
 
 		// TODO: multi-region
