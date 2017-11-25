99112ca125707adcbdda735ae56d50f9edd82621
TJ Holowaychuk <tj@vision-media.ca>
2017-11-25 11:02:35 -0800

    refactor logs: add leading zero

--- internal/logs/text/text.go
+++ internal/logs/text/text.go
@@ -158,11 +158,11 @@ var day = time.Hour * 24
 func formatDate(t time.Time) string {
 	switch d := time.Now().Sub(t); {
 	case d >= day*7:
-		return t.Format(`Jan 2` + dateSuffix(t) + ` 3:04:05pm`)
+		return t.Format(`Jan 2` + dateSuffix(t) + ` 03:04:05pm`)
 	case d >= day:
-		return t.Format(`2` + dateSuffix(t) + ` 3:04:05pm`)
+		return t.Format(`2` + dateSuffix(t) + ` 03:04:05pm`)
 	default:
-		return t.Format(`3:04:05pm`)
+		return t.Format(`03:04:05pm`)
 	}
 }
 
