57f4026bfcd48e5df65183e253ec36c6796d971b
TJ Holowaychuk <tj@vision-media.ca>
2017-12-13 08:38:39 -0800

    add confirmation to up.json creation

--- internal/setup/setup.go
+++ internal/setup/setup.go
@@ -3,6 +3,7 @@ package setup
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -15,7 +16,7 @@ var questions = []*survey.Question{
 	{
 		Name: "name",
 		Prompt: &survey.Input{
-			Message: "Name:",
+			Message: "Name of your project:",
 			Default: defaultName(),
 		},
 		Validate: survey.Required,
@@ -26,6 +27,20 @@ var questions = []*survey.Question{
 func Create() error {
 	var in struct {
 		Name string `json:"name"`
+	}
+
+	// confirm
+	var ok bool
+	err := survey.AskOne(&survey.Confirm{
+		Message: fmt.Sprintf("This directory has no up.json, create it?"),
+	}, &ok, nil)
+
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		return nil
 	}
 
 	if err := survey.Ask(questions, &in); err != nil {
