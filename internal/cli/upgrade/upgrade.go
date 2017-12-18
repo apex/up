8e90ab5a8c4ecd76a6ce5f96af9f200bcd5a4d00
TJ Holowaychuk <tj@vision-media.ca>
2017-12-18 08:45:06 -0800

    remove 0.0.0 hack for pro upgrade
    unnecessary in the Pro builds

--- internal/cli/upgrade/upgrade.go
+++ internal/cli/upgrade/upgrade.go
@@ -56,13 +56,10 @@ func init() {
 
 		// commercial edition
 		if t := config.GetActiveTeam(); t != nil {
-			// we pass 0.0.0 here beause the OSS
-			// binary should always upgrade to Pro
-			// regardless of versions matching.
 			p.Store = &apex.Store{
 				URL:       releasesAPI,
 				Product:   "up",
-				Version:   "0.0.0",
+				Version:   version,
 				Plan:      "pro",
 				AccessKey: t.Token,
 			}
