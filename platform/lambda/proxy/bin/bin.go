5e968e2e2188cd31e0e7edede7fb5f966d38b841
TJ Holowaychuk <tj@vision-media.ca>
2018-01-16 09:47:33 -0800

    misc

--- platform/lambda/proxy/bin/bin.go
+++ platform/lambda/proxy/bin/bin.go
@@ -1,4 +1,4 @@
-//go:generate sh -c "GOOS=linux GOARCH=amd64 go build -o up-proxy ../../../cmd/up-proxy/main.go"
+//go:generate sh -c "GOOS=linux GOARCH=amd64 go build -o up-proxy ../../../../cmd/up-proxy/main.go"
 //go:generate go-bindata -modtime 0 -pkg bin -o bin_assets.go .
 
 package bin
