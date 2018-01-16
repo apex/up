205701a45535d8ab0f66891b928d00fd0bd1dc16
TJ Holowaychuk <tj@vision-media.ca>
2018-01-15 22:02:13 -0800

    misc

--- platform/lambda/proxy/proxy.go
+++ platform/lambda/proxy/proxy.go
@@ -5,14 +5,25 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/pkg/errors"
 )
 
 // Start the proxy.
 func Start(h http.Handler) {
 	lambda.Start(func(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-		return events.APIGatewayProxyResponse{
-			Body:       "Hello from Native Go",
-			StatusCode: 200,
-		}, nil
+		req, err := NewRequest(e)
+		if err != nil {
+			return nil, errors.Wrap(err, "creating proxy request")
+		}
+
+		res := NewResponse()
+
+		h.ServeHTTP(res, req)
+
+		// return events.APIGatewayProxyResponse{
+		// 	Body:       "Hello from Native Go",
+		// 	StatusCode: 200,
+		// }, nil
+		return res.End(), nil
 	})
 }
