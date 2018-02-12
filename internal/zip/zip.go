bcbc56aeb8138cfc0eb2c181bf75432ee6ca7e83
TJ Holowaychuk <tj@vision-media.ca>
2018-02-12 14:19:35 -0800

    add blacklisting of gin-bin by default
    never need this in your build, sometimes it accidentally makes it in, especially now that .gitignore is not
    sourced by default

--- internal/zip/zip.go
+++ internal/zip/zip.go
@@ -37,7 +37,7 @@ func Build(dir string) (io.ReadCloser, *archive.Stats, error) {
 		strings.NewReader(".*\n"),
 		strings.NewReader("\n!node_modules/**\n!.pypath/**\n"),
 		upignore,
-		strings.NewReader("\n!main\n!server\n!_proxy.js\n!byline.js\n!up.json\n!pom.xml\n!build.gradle\n!project.clj\n"))
+		strings.NewReader("\n!main\n!server\n!_proxy.js\n!byline.js\n!up.json\n!pom.xml\n!build.gradle\n!project.clj\ngin-bin\n"))
 
 	filter, err := archive.FilterPatterns(r)
 	if err != nil {
