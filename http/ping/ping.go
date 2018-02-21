00481126fb331468b8ac7276b6823e863b845833
TJ Holowaychuk <tj@vision-media.ca>
2018-02-21 11:54:04 +0000

    add 1s sleep to /_ping endpoint
    so that concurrency is more accurate

--- http/ping/ping.go
+++ http/ping/ping.go
@@ -4,6 +4,7 @@ package ping
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/apex/up"
 )
@@ -12,6 +13,7 @@ import (
 func New(c *up.Config, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/_ping" {
+			time.Sleep(time.Second)
 			fmt.Fprintln(w, ":)")
 			return
 		}
