d95f6cb9ff4015d1481b017556d09fe5f6b90dbe
TJ Holowaychuk <tj@vision-media.ca>
2018-03-15 11:27:45 -0700

    add up pro features

--- config/lambda.go
+++ config/lambda.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"errors"
+	"time"
 )
 
 // defaultPolicy is the default function role policy.
@@ -47,6 +48,18 @@ type Lambda struct {
 
 	// VPC configuration.
 	VPC *VPC `json:"vpc"`
+
+	// Accelerate enables S3 acceleration.
+	Accelerate bool `json:"accelerate"`
+
+	// Warm enables active warming.
+	Warm *bool `json:"warm"`
+
+	// WarmCount is the number of containers to keep active.
+	WarmCount int `json:"warm_count"`
+
+	// WarmRate is the schedule for performing worming.
+	WarmRate Duration `json:"warm_rate"`
 }
 
 // Default implementation.
@@ -60,6 +73,14 @@ func (l *Lambda) Default() error {
 	}
 
 	l.Policy = append(l.Policy, defaultPolicy)
+
+	if l.WarmRate == 0 {
+		l.WarmRate = Duration(15 * time.Minute)
+	}
+
+	if l.WarmCount == 0 {
+		l.WarmCount = 15
+	}
 
 	return nil
 }
@@ -89,5 +110,17 @@ func (l *Lambda) Override(c *Config) {
 
 	if l.VPC != nil {
 		c.Lambda.VPC = l.VPC
+	}
+
+	if l.Warm != nil {
+		c.Lambda.Warm = l.Warm
+	}
+
+	if l.WarmCount > 0 {
+		c.Lambda.WarmCount = l.WarmCount
+	}
+
+	if l.WarmRate != 0 {
+		c.Lambda.WarmRate = l.WarmRate
 	}
 }
