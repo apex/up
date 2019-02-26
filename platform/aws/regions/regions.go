20b8344ba81ad6a97d83578bc9634ba5b813fd8e
TJ Holowaychuk <tj@vision-media.ca>
2019-02-26 18:30:10 +0000

    add regional endpoint support

--- platform/aws/regions/regions.go
+++ platform/aws/regions/regions.go
@@ -1,7 +1,31 @@
 // Package regions provides AWS region utilities.
 package regions
 
-import "path/filepath"
+import (
+	"fmt"
+	"path/filepath"
+)
+
+// hostedZoneIDs is a set of hosted zone ids for API Gateway.
+var hostedZoneIDs = map[string]string{
+	"us-east-2":      "ZOJJZC49E0EPZ",
+	"us-east-1":      "Z1UJRXOUMOOFQ8",
+	"us-west-1":      "Z2MUQ32089INYE",
+	"us-west-2":      "Z2OJLYMUO9EFXC",
+	"ap-south-1":     "Z3VO1THU9YC4UR",
+	"ap-northeast-3": "Z2YQB5RD63NC85",
+	"ap-northeast-2": "Z20JF4UZKIW1U8",
+	"ap-southeast-1": "ZL327KTPIQFUL",
+	"ap-southeast-2": "Z2RPCDW04V8134",
+	"ap-northeast-1": "Z1YSHQZHG15GKL",
+	"ca-central-1":   "Z19DQILCV0OWEC",
+	"eu-central-1":   "Z1U9ULNL0V5AJ3",
+	"eu-west-1":      "ZLY8HYME6SFDD",
+	"eu-west-2":      "ZJ5UAJN8Y3Z2Q",
+	"eu-west-3":      "Z3KY65QIEKYHQQ",
+	"eu-north-1":     "Z2YB950C88HT6D",
+	"sa-east-1":      "ZCMLWB8V5SYIT",
+}
 
 // IDs of regions.
 var IDs = []string{
@@ -70,4 +94,13 @@ func GetIdByName(name string) string {
 		}
 	}
 	return ""
+}
+
+// GetHostedZoneID returns a hosted zone id by region.
+func GetHostedZoneID(region string) string {
+	id, ok := hostedZoneIDs[region]
+	if !ok {
+		panic(fmt.Sprintf("region %q is not yet supported", region))
+	}
+	return id
 }
